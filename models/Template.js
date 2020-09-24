353a8b7933cc5d8a9ad8c63f71b8ba43c0ff8754
thanhtuancris <thanhtuancristiano@gmail.com>
2020-09-24 16:28:26 +0700

    update lastest 3

--- models/Template.js
+++ models/Template.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Template = new Schema({
-    Title: String,
-    Image: String,
-    Content: {type: String},
+    title: String,
+    image: String,
+    content: {type: String},
     demo: {type: String}
 });
 
